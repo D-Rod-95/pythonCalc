39d2490a34f031e98c49f247d5324027225e629a
D-Rod-95 <168130329+D-Rod-95@users.noreply.github.com>
2024-04-25 14:54:43 -0500

    Add files via upload

---
+++ python-calc.py
@@ -0,0 +1,29 @@
+# Program Name: Wk2P1_Daniel_Rodriguez.py
+# Student Name: Daniel Rodriguez
+# Course: ENTD220
+# Instructor: Osama Morad
+# Date: March 11, 2024
+# Copy Wrong: This is Dans Python
+
+# Below prompts the user to enter the first and second number and store it in a variable
+num1 = float(input("Enter your First number ---> "))
+
+num2 = float(input("Enter your Second number ---> "))
+
+# Below is all the calculations and store the result in a variable
+addition_result = num1 + num2
+
+subtraction_result = num1 - num2
+
+multiplication_result = num1 * num2
+
+division_result = num1 / num2
+
+# ----------------------------------
+
+print("The result of", num1, "+", num2, "=", addition_result)
+print("The result of", num1, "-", num2, "=", subtraction_result)
+print("The result of", num1, "*", num2, "=", multiplication_result)
+print("The result of", num1, "/", num2, "=", division_result)
+
+print("Thanks for using my calculator!")
